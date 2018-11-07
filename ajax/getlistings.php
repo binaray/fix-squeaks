2ee132201f000d71e953d7efb04ebeb05643f105
binaray@popon <raychengcx@live.com>
2018-11-07 12:52:34 +0800

    additional param for getlistings: listingId item name added as well

--- ajax/getlistings.php
+++ ajax/getlistings.php
@@ -1,26 +1,47 @@
 <?php
 require_once "../_config.php";
 
-$sql="SELECT * FROM Listings WHERE itemId = {$_GET["item"]} ORDER BY price";
-$result = $link->query($sql);
+if (isset($_GET["listingId"])){
+	$sql = "SELECT Listings.listingId, Listings.properties, Listings.price, Listings.quantity, Inventory.itemName FROM Listings INNER JOIN Inventory ON Listings.itemId = Inventory.itemId WHERE Listings.listingId = '{$_GET["listingId"]}'";
+	$result = $link->query($sql);
 
-$listings = array();
-while($row = $result->fetch_assoc()) {
-	if ($result->num_rows== 0) echo "No listings yet!";
-	else {
-		$listing = array();
-		$listing["listingId"] = $row["listingId"];
-		$listing["properties"] = $row["properties"];
-		$listing["price"] = $row["price"];
-		$listing["quantity"] = $row["quantity"];
-		if (isset($_GET["properties"])){
-			if ($listing["properties"]==$_GET["properties"])
-				array_push($listings, $listing);
+	while($row = $result->fetch_assoc()) {
+		if ($result->num_rows== 0) echo "No listings yet!";
+		else {
+			$listing = array();
+			$listing["listingId"] = $row["listingId"];
+			$listing["itemName"] = $row["itemName"];
+			$listing["properties"] = $row["properties"];
+			$listing["price"] = $row["price"];
+			$listing["quantity"] = $row["quantity"];
 		}
-		else array_push($listings, $listing);
 	}
+	mysqli_close($link);
+	echo json_encode($listing);
 }
-mysqli_close($link);
+else{
+	$sql = "SELECT Listings.listingId, Listings.properties, Listings.price, Listings.quantity, Inventory.itemName FROM Listings INNER JOIN Inventory ON Listings.itemId = Inventory.itemId WHERE Listings.itemId = '{$_GET["item"]}'";
+	//$sql="SELECT * FROM Listings WHERE itemId = {$_GET["item"]} ORDER BY price";
+	$result = $link->query($sql);
 
-echo json_encode($listings);
+	$listings = array();
+	while($row = $result->fetch_assoc()) {
+		if ($result->num_rows== 0) echo "No listings yet!";
+		else {
+			$listing = array();
+			$listing["listingId"] = $row["listingId"];
+			$listing["itemName"] = $row["itemName"];
+			$listing["properties"] = $row["properties"];
+			$listing["price"] = $row["price"];
+			$listing["quantity"] = $row["quantity"];
+			if (isset($_GET["properties"])){
+				if ($listing["properties"]==$_GET["properties"])
+					array_push($listings, $listing);
+			}
+			else array_push($listings, $listing);
+		}
+	}
+	mysqli_close($link);
+	echo json_encode($listings);
+}
 ?>
